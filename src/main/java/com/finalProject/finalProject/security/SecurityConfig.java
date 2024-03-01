2697594f1b1531c7c6650cf3e1c146de4cd7967d
manikittu810 <msanganabatla@csu.fullerton.edu>
2024-03-01 14:56:59 -0800

    security filter chain configuration updated

--- src/main/java/com/finalProject/finalProject/security/SecurityConfig.java
+++ src/main/java/com/finalProject/finalProject/security/SecurityConfig.java
@@ -3,23 +3,40 @@ package com.finalProject.finalProject.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests(authorize -> authorize
-                        .requestMatchers("/resources/**").permitAll()
-                        .requestMatchers("/public/**", "/registration", "/login").permitAll()
-                        .requestMatchers("/weather").permitAll()
-                        .requestMatchers("/farmer-list").authenticated()
-                        .anyRequest().authenticated()
-                )
-                .formLogin() // Use form-based authentication
-                .and()
-                .csrf().disable();
-        return http.build();
-    }
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeRequests(authorize -> authorize
+//                        .requestMatchers("/resources/**").permitAll()
+//                        .requestMatchers("/public/**", "/registration", "/login").permitAll()
+//                        .requestMatchers("/weather").permitAll()
+//                        .requestMatchers("/farmer-list").authenticated()
+//                        .anyRequest().authenticated()
+//                )
+//                .formLogin() // Use form-based authentication
+//                .and()
+//                .csrf().disable();
+//        return http.build();
+//    }
+@Bean
+SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+
+    http.authorizeHttpRequests(auth ->{
+        auth.anyRequest().authenticated();
+    });
+
+    http.sessionManagement(
+            session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+    );
+    //Allows all the requests to trigger
+    http.httpBasic();
+    http.csrf().disable();
+    return http.build();
+
+}
 }
